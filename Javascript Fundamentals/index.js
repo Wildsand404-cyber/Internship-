8e52cabb49c158a3080393ac40209040df32d2d1
Saif Cassim <66668230+Wildsand404-cyber@users.noreply.github.com>
2026-04-08 20:22:42 +0530

    Program to explore the queues in the event loop

--- Javascript Fundamentals/index.js
+++ Javascript Fundamentals/index.js
@@ -1,3 +1,32 @@
-const hello = "hello world";
+const fs = require('fs');
 
-console.log(hello);
+console.log("first line of code");
+console.log("call stack is empty now");
+
+const firstPriority = Process.nextTick(() => {
+  console.log('first priority');
+});
+
+const secondPriorty = Promise.resolve().then(() => {
+    console.log('second priority');
+});
+
+const thirdPriority = setTimeout(() => {
+    console.log("third priority"), 2000;
+});
+
+const fourthPriority = fs.readFile('./index.js', 'utf-8', (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('fourth priority');
+    }
+});  
+
+const fifthPriority = setImmediate(() => {
+    console.log('fifth priority');
+});
+
+const sixthPriority = readableStream.on(close, () => {
+    console.log('sixth priority');
+});
