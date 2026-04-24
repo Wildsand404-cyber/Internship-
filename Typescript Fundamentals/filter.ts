b2fe14fe7c0c534c919daa26290b3e7ccd857ba3
Saif Cassim <66668230+Wildsand404-cyber@users.noreply.github.com>
2026-04-24 11:50:09 +0530

    added TS generics to filter reimplementation

--- Typescript Fundamentals/filter.ts
+++ Typescript Fundamentals/filter.ts
@@ -1,5 +1,5 @@
-const filter: Function = (arr: number[], fn: Function): number[] => {
-    const filteredArr: number[] = [];
+const filter = <T>(arr: T[], fn: (arrIndex: T, index: number) => boolean): T[] => {
+    const filteredArr: T[] = [];
     for (let i: number = 0; i < arr.length; i++) {
         if (fn(arr[i], i)) {
             filteredArr.push(arr[i]);
@@ -11,5 +11,16 @@ const filter: Function = (arr: number[], fn: Function): number[] => {
 const arr: number[] = [0, 1, 2, 3, 4];
 const evenNumbers: number[] = filter(arr, (num: number) => num % 2 === 0);
 console.log(evenNumbers); // Output: [0, 2, 4]
+
+
+// const filter: Function = (arr: number[], fn: Function): number[] => {
+//     const filteredArr: number[] = [];
+//     for (let i: number = 0; i < arr.length; i++) {
+//         if (fn(arr[i], i)) {
+//             filteredArr.push(arr[i]);
+//         }
+//     }
+//     return filteredArr;
+// }
 
 
