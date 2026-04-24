25c345e0129af41c869922a6d2181643df458783
Saif Cassim <66668230+Wildsand404-cyber@users.noreply.github.com>
2026-04-24 11:28:33 +0530

    added TS generics to reduce reimplementation

--- Typescript Fundamentals/reduce.ts
+++ Typescript Fundamentals/reduce.ts
@@ -1,12 +1,22 @@
-const reduce = (nums: number[], fn: Function, init: number): number => {
-    let val: number = init;
-    for (let i: number = 0; i < nums.length; i++) {
-        val = fn(val, nums[i]);
+const reduce = <T, U>(nums: T[], fn: (acc: U, val: T) => U, init: U): U => {
+    let val: U = init;
+    for (const num of nums) {
+        val = fn(val, num);
     }
     return val;
 }
 
+// Usage with numbers
+
 const nums = [1, 2, 3, 4];
-const fn = function sum(acc: number, curr: number): number { return acc + curr; };
+const fn = function sum(a: number, b: number) { return a + b; };
 const init = 0;
-console.log(reduce(nums, fn, init)); // Output: 10
+
+console.log(reduce(nums, fn, init)); 
+
+// Usage with strings
+const strings = ["Hello", " ", "World", "!"];
+const concatFn = function concat(a: string, b: string) { return a + b; };
+const initStr = "";
+
+console.log(reduce(strings, concatFn, initStr));
