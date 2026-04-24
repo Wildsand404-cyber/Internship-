a365ae99c5ad0b7ddf940b2c0300b51f73530449
Saif Cassim <66668230+Wildsand404-cyber@users.noreply.github.com>
2026-04-24 11:39:21 +0530

    added TS generics to map() reimplementation

--- Typescript Fundamentals/map.ts
+++ Typescript Fundamentals/map.ts
@@ -1,5 +1,5 @@
-const customMap: Function = (arr: number[], callback: Function): number[] => {
-    const result: number[] = [];
+const customMap = <T, U>(arr: T[], callback: (num: T, index: number, array: T[]) => U): U[] => {
+    const result: U[] = [];
     for (let i: number = 0; i < arr.length; i++) {
         result.push(callback(arr[i], i, arr));
     }
@@ -9,3 +9,12 @@ const customMap: Function = (arr: number[], callback: Function): number[] => {
 const numbers: number[] = [10, 20, 30, 40, 50];
 const squaredNumbers: number[] = customMap(numbers, (num: number) => num * num);
 console.log(squaredNumbers); // Output: [100, 400, 900, 1600, 2500]
+
+
+// const customMap: Function = (arr: number[], callback: Function): number[] => {
+//     const result: number[] = [];
+//     for (let i: number = 0; i < arr.length; i++) {
+//         result.push(callback(arr[i], i, arr));
+//     }
+//     return result;
+// };
