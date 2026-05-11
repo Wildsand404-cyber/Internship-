4f198e694c0ce6727dd41129c6a249138950c906
Saif Cassim <66668230+Wildsand404-cyber@users.noreply.github.com>
2026-05-11 15:36:02 +0530

    added main ts file

---
+++ nestJS/main.ts
@@ -0,0 +1,18 @@
+// main.ts
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  // This turns on the validation for all endpoints
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,         // Strips away properties not in the DTO
+    forbidNonWhitelisted: true, // Throws error if extra properties are sent
+    transform: true,         // Automatically transforms payloads to DTO instances
+  }));
+
+  await app.listen(3000);
+}
+bootstrap();
